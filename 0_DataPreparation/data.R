9b0605855f599bdf0c7757ff614bf73fb0e47f4a
annapuu“ <annapuck@hotmail.de>
2023-12-19 12:33:29 +0100

    restore folder 0_DataPreparation

--- 0_DataPreparation/data.R
+++ 0_DataPreparation/data.R
@@ -4,20 +4,25 @@ library("dplyr")
 library("lubridate")
 
 # load all datasets
-d1 <- read_csv("0_DataPreparation/umsatzdaten_gekuerzt.csv")
+d1 <- read_csv("0_DataPreparation/train_kaggle.csv")
 d2 <- read_csv("0_DataPreparation/kiwo.csv")
 d3 <- read_csv("0_DataPreparation/wetter.csv")
 d4 <- read_csv("0_DataPreparation/schulferien.csv")
 
 # create a single dataset (tibble)
 data <- d1 %>%
-  left_join(d2, by = "Datum") %>%
-  left_join(d3, by = "Datum") %>%
-  left_join(d4, by = "Datum")
+  full_join(d2, by = "Datum") %>%
+  full_join(d3, by = "Datum") %>%
+  full_join(d4, by = "Datum")
 
 # create a new column for the variable weekend
 data <- data %>%
   mutate(Wochenende = ifelse(wday(Datum) %in% c(1, 7), 1, 0))
 
+# fill missing values in columns KielerWoche and Schulferien with zero
+data_filled <- data %>%
+  mutate(KielerWoche = ifelse(is.na(KielerWoche), 0, KielerWoche),
+         Schulferien = ifelse(is.na(Schulferien), 0, Schulferien))
+
 # save tibble as a csv-file
-write.csv(data, file = "0_DataPreparation/data.csv", row.names = FALSE)
+write.csv(data_filled, file = "0_DataPreparation/data.csv", row.names = FALSE)
