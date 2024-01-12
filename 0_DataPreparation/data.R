057ddf781f8d66bf34607be1a1e010b9e27a1317
annapuu“ <annapuck@hotmail.de>
2024-01-12 16:44:14 +0100

    add 2 new variables, delete now unneeded files

--- 0_DataPreparation/data.R
+++ 0_DataPreparation/data.R
@@ -8,14 +8,14 @@ d1 <- read_csv("0_DataPreparation/train_kaggle.csv")
 d2 <- read_csv("0_DataPreparation/kiwo.csv")
 d3 <- read_csv("0_DataPreparation/wetter.csv")
 d4 <- read_csv("0_DataPreparation/schulferien.csv")
-# d5 <- read_csv("0_DataPreparation/feiertage_compact.csv")
+d5 <- read_csv("0_DataPreparation/feiertage_compact.csv")
 
 # create a single dataset (tibble)
 data <- d1 %>%
   full_join(d2, by = "Datum") %>%
   full_join(d3, by = "Datum") %>%
-  full_join(d4, by = "Datum") # %>%
-  # full_join(d5, by = "Datum")
+  full_join(d4, by = "Datum") %>%
+  full_join(d5, by = "Datum")
 
 # create a new column for the variable weekend
 data <- data %>%
@@ -26,5 +26,14 @@ data_filled <- data %>%
   mutate(KielerWoche = ifelse(is.na(KielerWoche), 0, KielerWoche),
          Schulferien = ifelse(is.na(Schulferien), 0, Schulferien))
 
+#creating another variable named season
+data_finished <- data_filled %>%
+  mutate(Season = ifelse(month(Datum) %in% c(3, 4, 5), "Spring",
+                         ifelse(month(Datum) %in% c(6, 7, 8), "Summer",
+                                ifelse(month(Datum) %in% c(9, 10, 11), "Fall", "Winter"))))
+
+# check
+print(unique(data_finished$Season))
+
 # save tibble as a csv-file
-write.csv(data_filled, file = "0_DataPreparation/data.csv", row.names = FALSE)
+write.csv(data_finished, file = "0_DataPreparation/data.csv", row.names = FALSE)
